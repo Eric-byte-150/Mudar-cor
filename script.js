6cc9306215675d832c311e25f85618217d0763e3
Eric-byte-150 <ericribeiro756@gmail.com>
2023-01-18 19:22:20 -0300

    oii

---
+++ script.js
@@ -0,0 +1,19 @@
+function ts(){
+    var a = document.getElementById('ex').value;
+    var b = a.length;
+    var p=document.getElementById('p').value
+   if(b==0){
+    document.getElementById('ex').style.background ="red";
+    alert('Escreva seu nome mane')
+    document.getElementById('p').innerHTML='cu'
+    
+   }
+       
+
+
+
+   
+
+
+   
+}
